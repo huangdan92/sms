1f672db28c2d34e41fbc2936951ba030e0d8d41e
huangdan92 <huangdan921@qq.com>
2025-11-15 20:38:40 +0800

    优化代码

--- sims/utils.py
+++ sims/utils.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from PIL import Image
-from io import BytesIO
 from selenium.webdriver.chrome.service import Service
 import time
 import os
--- sims/views.py
+++ sims/views.py
@@ -2,6 +2,8 @@ import MySQLdb
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import re
+from django.db.models import Q
+from sims.models import Student
 
 # Create your views here.
 # 任务信息列表处理函数
@@ -9,37 +11,40 @@ from sims.utils import capture_scroll_screenshot, extract_prometheus_with_regex,
 
 
 def index(request):
-    student_no = request.GET.get('student_no', '')
-    student_name = request.GET.get('student_name', '')
-    job_name = request.GET.get('job_name', '')
-    selected_targets_str = request.GET.get('selected_targets_str', '')
-    custom_config_content = request.GET.get('custom_config_content', '')
-    custom_selected_targets_str = request.GET.get('custom_selected_targets_str', '')
+    student_no = request.GET.get('student_no', '').strip()
+    student_name = request.GET.get('student_name', '').strip()
+    job_name = request.GET.get('job_name', '').strip()
+    selected_targets_str = request.GET.get('selected_targets_str', '').strip()
+    custom_config_content = request.GET.get('custom_config_content', '').strip()
+    custom_selected_targets_str = request.GET.get('custom_selected_targets_str', '').strip()
 
-    sql = "SELECT id,student_no,student_name,job_name,selected_targets_str,custom_config_content,custom_selected_targets_str FROM sims_student WHERE 1=1 "
-    if student_no.strip() != '':
-        sql = sql + " and student_no = '" + student_no + "'"
-    if student_name.strip() != '':
-        sql = sql + " and student_name = '" + student_name + "'"
-    if job_name.strip() != '':
-        sql = sql + " and job_name = '" + job_name + "'"
-    if selected_targets_str.strip() != '':
-        sql = sql + " and selected_targets_str = '" + selected_targets_str + "'"
-    if custom_config_content.strip() != '':
-        sql = sql + " and custom_config_content = '" + custom_config_content + "'"
-    if custom_selected_targets_str.strip() != '':
-        sql = sql + " and custom_selected_targets_str = '" + custom_selected_targets_str + "'"
+    # 构建查询条件
+    queries = Q()
 
-    print(sql)
-    conn = MySQLdb.connect(host="172.20.10.5", user="root", passwd="rootroot", db="sms", charset='utf8mb4')
-    with conn.cursor(cursorclass=MySQLdb.cursors.DictCursor) as cursor:
-        cursor.execute(sql)
-        students = cursor.fetchall()
-    return render(request, 'student/index.html', {'students': students,
-                                                  'student_name': student_name, 'student_no': student_no,
-                                                  'job_name': job_name, 'selected_targets_str': selected_targets_str,
-                                                  'custom_config_content': custom_config_content,
-                                                  'custom_selected_targets_str': custom_selected_targets_str})
+    if student_no:
+        queries &= Q(student_no=student_no)
+    if student_name:
+        queries &= Q(student_name=student_name)
+    if job_name:
+        queries &= Q(job_name=job_name)
+    if selected_targets_str:
+        queries &= Q(selected_targets_str=selected_targets_str)
+    if custom_config_content:
+        queries &= Q(custom_config_content=custom_config_content)
+    if custom_selected_targets_str:
+        queries &= Q(custom_selected_targets_str=custom_selected_targets_str)
+
+    students = Student.objects.filter(queries).order_by('-id')
+
+    return render(request, 'student/index.html', {
+        'students': students,
+        'student_name': student_name,
+        'student_no': student_no,
+        'job_name': job_name,
+        'selected_targets_str': selected_targets_str,
+        'custom_config_content': custom_config_content,
+        'custom_selected_targets_str': custom_selected_targets_str
+    })
 
 
 # 任务信息新增处理函数
